3e3a9e321a0f708f4edd9757477cea7089226132
Leonam Monteiro <devleonammonteiro@gmail.com>
2025-02-27 02:00:41 -0300

    Added new folder: Modulo8

---
+++ Modulo8/TarefaCalculoMedia/src/br/com/mod8/Media.java
@@ -0,0 +1,13 @@
+package br.com.mod8;
+
+public class Media {
+
+    public static void main(String[] args) {
+        double nota1 = 9;
+        double nota2 = 7.5;
+        double nota3 = 10;
+        double nota4 = 5.5;
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+        System.out.println("A média é: " + media);
+    }
+}
